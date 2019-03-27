c864f614e0631feaa410f32987f624334dd54f88
Leonel <lolo042@hotmail.com>
2019-03-27 17:59:44 -0300

    test de invasor

---
+++ test/testInvasor.ts
@@ -0,0 +1,25 @@
+import { Invasor } from "../Invasor";
+import { Nave } from "../Nave";
+import {expect} from "chai";
+
+describe("destruir y chocar nave", function(){
+    it("Cuando choca debe dar igual a 0 la vida de la nave", function(){
+        var nave = new Nave(100, 90);
+        var invasor = new Invasor(100, 50);
+
+        invasor.chocarNave(nave);
+
+        expect(nave.getVida()).to.equal(0);
+
+    })
+    it("Cuando destruye debe dar 40 la vida de la nave", function(){
+
+        var nave = new Nave(100, 90);
+        var invasor = new Invasor(100, 50);
+
+        invasor.destruirNave(nave);
+
+        expect(nave.getVida()).to.equal(40);
+
+    })
+})
