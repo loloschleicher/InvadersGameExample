316b1f956df39c5f1671d2a07ee18e38ad646983
Leonel <lolo042@hotmail.com>
2019-03-27 18:00:30 -0300

    implementacion de invasor.ts

--- Invasor.ts
+++ Invasor.ts
@@ -1,9 +1,36 @@
-class Invasores{
+import {Nave} from "./Nave";
+export class Invasor{
     vida: number;
     velocidad: number;
 
-    constructor(vida: number, velocidad:number){
-        this.vida = vida;
-        this.velocidad = velocidad;
+    constructor(vida: number, velocidad: number){
+        this.setVida(vida);
+        this.setVelocidad(velocidad);
+    }
+
+    setVida(vida: number){
+        this.vida = vida; 
+    }
+
+    setVelocidad(velocidad: number){
+        this.velocidad = velocidad; 
+    }
+
+    getVida():number{
+        return this.vida; 
+    }
+
+    getVelocidad():number{
+        return this.velocidad; 
+    }
+
+    chocarNave(nave: Nave){
+        nave.setVida(0);
+    }
+
+    destruirNave(nave: Nave){
+        var nuevoDanio = (this.getVelocidad() * 20 / 100) + this.getVelocidad();     
+        var nuevaVida = nave.getVida() - nuevoDanio;
+        nave.setVida(nuevaVida);       
     }
 }
