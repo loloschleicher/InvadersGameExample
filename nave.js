f5ba2d053b973f81aab8ee6e4b33a007d62692aa
Leonel <lolo042@hotmail.com>
2019-03-27 17:51:48 -0300

    compilado de nave a js

--- nave.js
+++ nave.js
@@ -2,11 +2,25 @@
 exports.__esModule = true;
 var Nave = /** @class */ (function () {
     function Nave(vida, velocidad) {
-        this.vida = vida;
-        this.velocidad = velocidad;
+        this.setVida(vida);
+        this.setVelocidad(velocidad);
     }
-    Nave.prototype.destruir = function (nombre) {
-        return 0;
+    Nave.prototype.setVida = function (vida) {
+        this.vida = vida;
+    };
+    Nave.prototype.setVelocidad = function (velocidad) {
+        this.velocidad = velocidad;
+    };
+    Nave.prototype.getVida = function () {
+        return this.vida;
+    };
+    Nave.prototype.getVelocidad = function () {
+        return this.velocidad;
+    };
+    Nave.prototype.destruir = function (invasor) {
+        var vidaPerdida = (invasor.getVelocidad() * this.getVelocidad()) / 100;
+        var nuevaVida = invasor.getVida() - vidaPerdida;
+        invasor.setVida(nuevaVida);
     };
     return Nave;
 }());
