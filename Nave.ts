d239621f0d85e1af24b4369f0f202a93136dd1b9
Leonel <lolo042@hotmail.com>
2019-03-27 17:51:18 -0300

    implementacion nave.ts

--- Nave.ts
+++ Nave.ts
@@ -1,17 +1,34 @@
-
+import { Invasor } from "./Invasor";
 
 export class Nave{
     vida: number;
     velocidad: number;
 
     constructor(vida: number, velocidad: number){
-        this.vida = vida;
-        this.velocidad = velocidad;
+        this.setVida(vida);
+        this.setVelocidad(velocidad);
     }
 
-    destruir(nombre: string){
-        return 0;
-    
+    setVida(vida: number){
+        this.vida = vida; 
+    }
+
+    setVelocidad(velocidad: number){
+        this.velocidad = velocidad; 
+    }
+
+    getVida():number{
+        return this.vida; 
+    }
+
+    getVelocidad():number{
+        return this.velocidad; 
+    }
+
+    destruir(invasor: Invasor){
+        var vidaPerdida = (invasor.getVelocidad() * this.getVelocidad()) / 100;
+        var nuevaVida = invasor.getVida() - vidaPerdida;
+        invasor.setVida(nuevaVida); 
     }
 
 
