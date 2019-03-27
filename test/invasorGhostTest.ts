e559974e3f550a577b9a0b75fd2264f10753fa68
Leonel <lolo042@hotmail.com>
2019-03-27 20:49:53 -0300

    Test de destruir nave del invasor ghost

--- test/invasorGhostTest.ts
+++ test/invasorGhostTest.ts
@@ -23,4 +23,15 @@ describe("Invasor Ghost , destruir y chocar nave", function(){
         expect(nave.getVida()).to.equal(100);
 
     })
+
+    it("Cuando ghost destruye la nave deberia dar 55", function(){
+
+        var nave = new Nave(100, 90);
+        var invasorGhost = new InvasorGhost(100, 50);
+
+        invasorGhost.destruirNave(nave);
+
+        expect(nave.getVida()).to.equal(40);
+
+    })
 })
